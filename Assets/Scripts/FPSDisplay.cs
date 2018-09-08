3ebebc661b857a22252bb7863bc77612462ff36b
Kellers176 <34107413+Kellers176@users.noreply.github.com>
2018-09-08 18:58:09 -0400

    Initial Commit

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour {
+
+    public Text highestFPSLabel, averageFPSLabel, lowestFPSLabel;
+    FPSCounter fpsCounter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+
+       fpsCounter = GetComponent<FPSCounter>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+       // fpsLabel.text = fpsCounter.FPS.ToString();
+       // fpsLabel.text = Mathf.Clamp(fpsCounter.FPS, 0, 99).ToString();
+
+        highestFPSLabel.text = Mathf.Clamp(fpsCounter.HighestFPS, 0, 99).ToString();
+        averageFPSLabel.text = Mathf.Clamp(fpsCounter.AverageFPS, 0, 99).ToString();
+        lowestFPSLabel.text = Mathf.Clamp(fpsCounter.LowestFPS, 0, 99).ToString();
+    }
+}
---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nucleon : MonoBehaviour {
+
+    public float attractionForce;
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.localPosition * -attractionForce);
+    }
+}
